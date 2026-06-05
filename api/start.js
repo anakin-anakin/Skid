29b1c52116485b1b81340625ba2ed96e72b266d7
anakin-anakin <260070196+anakin-anakin@users.noreply.github.com>
2026-06-05 22:40:06 +0300

    Update start.js

--- api/start.js
+++ api/start.js
@@ -1,10 +1,14 @@
 export default function handler(req, res) {
-    const placeId = req.query.placeId;
-    const jobId = req.query.gameInstanceId;
+    const { placeId, gameInstanceId } = req.query;
+    if (!placeId || !gameInstanceId) return res.status(400).send("Missing IDs");
 
-    if (!placeId || !jobId) {
-        return res.status(400).send("Smth missing nga");
-    }
-
-    return res.redirect(`roblox://placeId=${placeId}&gameInstanceId=${jobId}`);
+    const url = `roblox://placeId=${placeId}&gameInstanceId=${gameInstanceId}`;
+    res.setHeader('Content-Type', 'text/html');
+    return res.status(200).send(`
+        <title>QUYX Hub Joiner</title>
+        <meta property="og:title" content="QUYX Hub Joiner">
+        <meta property="og:description" content="Join your victim via this link gng">
+        <meta property="og:theme-color" content="#1D4ED8">
+        <script>window.location.href="${url}"</script>
+    `);
 }
